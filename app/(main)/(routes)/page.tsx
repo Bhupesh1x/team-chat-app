f2aa662c5e299c80a3b5dee6d307001b07e51422
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2023-10-26 19:13:07 +0530

    Toogle dark and light mode changes.

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
 
+import { ToggleMode } from "@/components/ToggleMode";
+
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ToggleMode />
     </div>
   );
 }
