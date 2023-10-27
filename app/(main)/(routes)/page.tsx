489f43ace51e1a93ae580dd89b87edc07725cb40
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2023-10-27 16:49:59 +0530

    Initial profile setup changes.

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,12 +1,30 @@
+import { redirect } from "next/navigation";
+
 import { UserButton } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
 
 import { ToggleMode } from "@/components/ToggleMode";
 
-export default function Home() {
+export default async function Home() {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) return redirect(`/servers/${server.id}`);
+
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
-      <ToggleMode />
+      <h1>Create a server</h1>
     </div>
   );
 }
---
+++ lib/initial-profile.ts
@@ -0,0 +1,28 @@
+import { db } from "./db";
+
+import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+
+export async function initialProfile() {
+  const user = await currentUser();
+
+  if (!user) return redirectToSignIn();
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (profile) return profile;
+
+  const userProfile = await db.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
+
+  return userProfile;
+}
