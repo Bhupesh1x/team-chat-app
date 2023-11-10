cbd77f1d766e2fdd38d2f9c48c8fb48b97a68033
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2023-11-10 19:59:27 +0530

    edit individual message frontend changes.

--- components/chat/ChatItem.tsx
+++ components/chat/ChatItem.tsx
@@ -1,13 +1,28 @@
 "use client";
 
+import * as z from "zod";
+import qs from "query-string";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Member, MemberRole, Profile } from "@prisma/client";
-import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
+import {
+  Edit,
+  FileIcon,
+  Loader2,
+  ShieldAlert,
+  ShieldCheck,
+  Trash,
+} from "lucide-react";
 
 import UserAvatar from "../shared/UserAvatar";
 import { ActionTooltip } from "../shared/ActionTooltip";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import axios from "axios";
 
 type Props = {
   id: string;
@@ -29,6 +44,10 @@ const roleIconMap = {
   MODERATOR: <ShieldCheck className="h-4 w-4 text-indigo-500 cursor-pointer" />,
   ADMIN: <ShieldAlert className="h-4 w-4 text-rose-500 cursor-pointer" />,
 };
+
+const formSchema = z.object({
+  content: z.string().min(1),
+});
 
 function ChatItem({
   id,
@@ -53,6 +72,47 @@ function ChatItem({
   const canEditMessgae = !deleted && isOwner && !fileUrl;
   const isPdf = fileType === "pdf" && fileUrl;
   const isImage = fileUrl && !isPdf;
+
+  useEffect(() => {
+    const handleKeyDown = (e: any) => {
+      if (e.key === "Escape" || e.keyCode === 27) {
+        setIsEditing(false);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: content,
+    },
+  });
+
+  useEffect(() => {
+    form.reset({
+      content: content,
+    });
+  }, [content]);
+
+  const { isSubmitting } = form.formState;
+
+  const onSubmit = async (value: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({
+        url: `${socketUrl}/${id}`,
+        query: socketQuery,
+      });
+
+      await axios.patch(url, value);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="relative group flex items-center w-full p-4 transition bg-black/5">
@@ -116,6 +176,43 @@ function ChatItem({
                 </span>
               )}
             </p>
+          )}
+          {!fileUrl && isEditing && (
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className=" flex items-centerw-full gap-x-2 pt-2"
+              >
+                <FormField
+                  name="content"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormControl>
+                        <div className="relative w-full">
+                          <Input
+                            disabled={isSubmitting}
+                            className="p-2 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                            placeholder="Edited Message"
+                            {...field}
+                          />
+                        </div>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <Button disabled={isSubmitting} size="sm" variant="primary">
+                  {isSubmitting ? (
+                    <Loader2 className="h-4 w-4 animate-spin" />
+                  ) : (
+                    "Save"
+                  )}
+                </Button>
+              </form>
+              <span className="text-[10px] mt-1 text-zinc-400">
+                Press escape to cancel,enter to save
+              </span>
+            </Form>
           )}
         </div>
       </div>
